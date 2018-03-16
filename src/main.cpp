09a011da143c1e1a2f03a6f38737a290f2f5bbad
Mark Gentles <Mark.Gentles@hotmail.co.uk>
2018-03-16 22:41:01 +0000

    Build + Link to GLFW and GLEW

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #define GLEW_STATIC
 #include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main(char*, char**) {
 			
 	glewInit();
+	glfwInit();
 	std::cout << "Hello World!" << std::endl;
 	std::cin.get();
 	return 0;
