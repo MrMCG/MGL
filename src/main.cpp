7c2022565f9a3a2b25c180173595d341655116f7
Mark Gentles <Mark.Gentles@hotmail.co.uk>
2018-03-16 21:49:59 +0000

    Builds against glew-2.1.0_static

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
 
 int main(char*, char**) {
 			
+	glewInit();
 	std::cout << "Hello World!" << std::endl;
 	std::cin.get();
 	return 0;
