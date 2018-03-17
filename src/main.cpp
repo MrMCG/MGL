8070f933df347b143bb029a7f15017e99d2dcc10
Mark Gentles <Mark.Gentles@hotmail.co.uk>
2018-03-17 00:25:27 +0000

    Created basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,64 @@
-#include <iostream>
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <memory>
+#include <iostream>
+
+void APIENTRY MessageCallback(GLenum source,
+	GLenum type,
+	GLuint id,
+	GLenum severity,
+	GLsizei length,
+	const GLchar* message,
+	const void* userParam)
+{
+	fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+		(type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
+		type, severity, message);
+}
+
 int main(char*, char**) {
 			
-	glewInit();
+	glEnable(GL_DEBUG_OUTPUT);
 	glfwInit();
-	std::cout << "Hello World!" << std::endl;
-	std::cin.get();
-	return 0;
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+
+	auto window = glfwCreateWindow(800, 600, "First Window", nullptr, nullptr);
+	
+	if (!window) {
+		std::cout << "failed to create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	std::cout << "GL version: " << glGetString(GL_VERSION) << std::endl;
+
+	glewInit();
+
+	if (glDebugMessageCallback) {
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageCallback((GLDEBUGPROC)MessageCallback, 0);
+		std::cout << "glDebugMessageCallback enabled" << std::endl;
+	}
+
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glViewport(0, 0, 800, 600);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
